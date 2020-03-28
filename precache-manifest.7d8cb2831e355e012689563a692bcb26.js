99ab74795aeee92076717f09de5f4059bcb92d85
Marin Cenusa <marin_cenusa@netex.ro>
2020-03-28 21:29:26 +0200

    Initial commit

---
+++ precache-manifest.7d8cb2831e355e012689563a692bcb26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160945fc274b5404d92b35261ce0af7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab18cf6a16af848b2166",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "818953af2702db0393cc",
+    "url": "/static/js/2.f2d36ac2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2d36ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab18cf6a16af848b2166",
+    "url": "/static/js/main.abe48db4.chunk.js"
+  },
+  {
+    "revision": "12f8594f7e16ea606c8d",
+    "url": "/static/js/runtime-main.c84ac9e4.js"
+  }
+]);
